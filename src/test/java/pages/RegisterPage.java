7baaf5f1ae747edc85005e716757565956fa1dfd
dereay <74974843+dereay@users.noreply.github.com>
2020-12-15 09:44:23 +0100

    Add registerInvalidPassword

--- src/test/java/pages/RegisterPage.java
+++ src/test/java/pages/RegisterPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -56,6 +57,10 @@ public class RegisterPage {
         System.err.println("INFO: Will fill address as " + Values.getAddress());
         Thread.sleep(1200);
 
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        jse.executeScript("window.scrollBy(0,250)");
+
+
         WebElement email = driver.findElement(By.xpath(xpath + "'email']"));
         email.sendKeys(Values.getEmail());
         System.err.println("INFO: Will fill email as " + Values.getEmail());
@@ -73,11 +78,89 @@ public class RegisterPage {
 
         driver.findElement(By.xpath("//button[@class='btn btn-primary mt-4']")).click();
 
+        driver.findElement(By.xpath("//li[@placement='bottom-right']")).click();
+        Thread.sleep(1500);
+        driver.findElement(By.linkText("Çıkış Yap")).click();
+
+
     }
 
-    /*
-    şifreyi yanlış girme ve
-    VKN aynı olma metodları da eklenecek
-     */
+    @Test
+    public static void registerInvalidPassword() throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "chromeDriver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        String xpath = "//input[@formcontrolname=";
+
+        driver.get("https://muhasebe-deploy.herokuapp.com/login");
+        Thread.sleep(2500);
+
+        driver.findElement(By.xpath("//a[@class = 'nav-link nav-link-icon']")).click();
+
+        WebElement VKNo = driver.findElement(By.xpath("//input[@placeholder='VKN']"));
+        VKNo.sendKeys(Values.getVKN());
+        System.err.println("INFO: Will fill VKN as " + Values.getVKN());
+        Thread.sleep(1200);
+
+        WebElement fullname1 = driver.findElement(By.xpath(xpath + "'name']"));
+        fullname1.sendKeys(Values.getFullname());
+        System.err.println("INFO: Will fill fullname as " + Values.getFullname());
+        Thread.sleep(1200);
+
+        WebElement company = driver.findElement(By.xpath(xpath + "'company']"));
+        company.sendKeys(Values.getCompany());
+        System.err.println("INFO: Will fill company as " + Values.getCompany());
+        Thread.sleep(1200);
+
+        WebElement phone = driver.findElement(By.xpath(xpath + "'phoneNumber']"));
+        phone.sendKeys(Values.getPhone());
+        System.err.println("INFO: Will fill phone as " + Values.getPhone());
+        Thread.sleep(1200);
+
+        WebElement address = driver.findElement(By.xpath(xpath + "'address']"));
+        address.sendKeys(Values.getAddress());
+        System.err.println("INFO: Will fill address as " + Values.getAddress());
+        Thread.sleep(1200);
+
+        WebElement email = driver.findElement(By.xpath(xpath + "'email']"));
+        email.sendKeys(Values.getEmail());
+        System.err.println("INFO: Will fill email as " + Values.getEmail());
+        Thread.sleep(1200);
+
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        jse.executeScript("window.scrollBy(0,250)");
+
+        WebElement password = driver.findElement(By.xpath(xpath + "'password']"));
+        password.sendKeys(Values.getPassword());
+        System.err.println("INFO: Will fill password as " + Values.getPassword());
+        Thread.sleep(1200);
+
+        WebElement confirmPass = driver.findElement(By.xpath(xpath + "'confirmPass']"));
+        confirmPass.sendKeys("1254");
+        System.err.println("INFO: Will fill confirmPass as wrong");
+        Thread.sleep(1200);
+
+        if(Values.getPassword().equals(password)) {
+            driver.findElement(By.xpath("//button[@class='btn btn-primary mt-4']")).click();
+
+            System.err.println("INFO: Password entered correctly");
+            driver.findElement(By.xpath("//li[@placement='bottom-right']")).click();
+            Thread.sleep(1500);
+            driver.findElement(By.linkText("Çıkış Yap")).click();
+        }
+        else {
+            driver.findElement(By.xpath("//button[@class='btn btn-primary mt-4']")).click();
+
+            System.err.println("INFO: Password entered incorrectly");
+            String fail = driver.findElement(By.xpath("//div[@class='alert alert-text']")).getText();
+            System.out.println(fail);
+
+        }
+
+    }
+    //VKN numarasını aynı girme
+
 
 }
