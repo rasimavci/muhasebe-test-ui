87b91cef9a2a72b5dd8cbaef0da66c99160e6f5f
dereay <74974843+dereay@users.noreply.github.com>
2020-12-18 11:51:21 +0100

    Add change and restore Name,Company,PhoneNumber

--- src/test/java/pages/AdminTablesPage.java
+++ src/test/java/pages/AdminTablesPage.java
@@ -123,8 +123,272 @@ public class AdminTablesPage {
         driver.close();
 
     }
-
     @Test(priority = 3)
+    public static void changeName() throws InterruptedException {
+        System.setProperty(Values.getChromeDriver(), Values.getChromeDriverPath());
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://muhasebe-deploy.herokuapp.com/admin");
+        if (driver.getCurrentUrl().endsWith("admin")) {
+            System.out.println(Values.getUrlEndsWithLoginMessage());
+        }
+        else {
+            System.out.println(Values.getUrlDoesNotEndsWithLoginMessage());
+        }
+        driver.findElement(By.xpath(xpath + "'username']")).sendKeys("7840266351");
+        driver.findElement(By.xpath(xpath + "'password']")).sendKeys(Values.getValidPassword());
+        driver.findElement(By.xpath(Values.getLoginButtonXpath())).click();
+        Thread.sleep(3000);
+        if (driver.getCurrentUrl().endsWith("admin-dashboard")) {
+            System.out.println(Values.getSuccessfulLoginMessage());
+        }
+        else {
+            System.out.println(Values.getLoginErrorMessage());
+        }
+        driver.findElement(By.xpath("//a[@class='nav-link']")).click();
+        Thread.sleep(1200);
+        driver.findElement(By.xpath("(//button[@class='btn btn-sm btn-primary'])[1]")).click();
+        Thread.sleep(1200);
+        driver.findElement(By.xpath("//select[@class='btn btn-outline-primary ng-untouched ng-pristine ng-valid']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@value='name']")).click();
+        Thread.sleep(1500);
+        driver.findElement(By.xpath("//*[@class='form-control form-control-alternative']")).clear();
+        Thread.sleep(1500);
+        driver.findElement(By.xpath("//*[@class='form-control form-control-alternative']")).sendKeys("Aysu");
+        Thread.sleep(1500);
+
+        driver.findElement(By.xpath("//button[@class='btn btn-outline-dark']")).click();
+
+        driver.findElement(By.xpath(Values.getMenuButtonXpath())).click();
+        Thread.sleep(1500);
+        driver.findElement(By.xpath(Values.getLogoutButtonXpath())).click();
+        driver.close();
+
+    }
+    @Test(priority = 4)
+    public static void restoreName() throws InterruptedException {
+        System.setProperty(Values.getChromeDriver(), Values.getChromeDriverPath());
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://muhasebe-deploy.herokuapp.com/admin");
+        if (driver.getCurrentUrl().endsWith("admin")) {
+            System.out.println(Values.getUrlEndsWithLoginMessage());
+        }
+        else {
+            System.out.println(Values.getUrlDoesNotEndsWithLoginMessage());
+        }
+        driver.findElement(By.xpath(xpath + "'username']")).sendKeys("7840266351");
+        driver.findElement(By.xpath(xpath + "'password']")).sendKeys(Values.getValidPassword());
+        driver.findElement(By.xpath(Values.getLoginButtonXpath())).click();
+        Thread.sleep(3000);
+        if (driver.getCurrentUrl().endsWith("admin-dashboard")) {
+            System.out.println(Values.getSuccessfulLoginMessage());
+        }
+        else {
+            System.out.println(Values.getLoginErrorMessage());
+        }
+        driver.findElement(By.xpath("//a[@class='nav-link']")).click();
+        Thread.sleep(1200);
+        driver.findElement(By.xpath("(//button[@class='btn btn-sm btn-primary'])[1]")).click();
+        Thread.sleep(1200);
+        driver.findElement(By.xpath("//select[@class='btn btn-outline-primary ng-untouched ng-pristine ng-valid']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@value='name']")).click();
+        Thread.sleep(1500);
+        driver.findElement(By.xpath("//*[@class='form-control form-control-alternative']")).clear();
+        Thread.sleep(1500);
+        driver.findElement(By.xpath("//*[@class='form-control form-control-alternative']")).sendKeys("Tohan");
+        Thread.sleep(1500);
+
+        driver.findElement(By.xpath("//button[@class='btn btn-outline-dark']")).click();
+
+        driver.findElement(By.xpath(Values.getMenuButtonXpath())).click();
+        Thread.sleep(1500);
+        driver.findElement(By.xpath(Values.getLogoutButtonXpath())).click();
+        driver.close();
+
+    }
+    @Test(priority = 5)
+    public static void changeCompany() throws InterruptedException {
+        System.setProperty(Values.getChromeDriver(), Values.getChromeDriverPath());
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://muhasebe-deploy.herokuapp.com/admin");
+        if (driver.getCurrentUrl().endsWith("admin")) {
+            System.out.println(Values.getUrlEndsWithLoginMessage());
+        }
+        else {
+            System.out.println(Values.getUrlDoesNotEndsWithLoginMessage());
+        }
+        driver.findElement(By.xpath(xpath + "'username']")).sendKeys("7840266351");
+        driver.findElement(By.xpath(xpath + "'password']")).sendKeys(Values.getValidPassword());
+        driver.findElement(By.xpath(Values.getLoginButtonXpath())).click();
+        Thread.sleep(3000);
+        if (driver.getCurrentUrl().endsWith("admin-dashboard")) {
+            System.out.println(Values.getSuccessfulLoginMessage());
+        }
+        else {
+            System.out.println(Values.getLoginErrorMessage());
+        }
+        driver.findElement(By.xpath("//a[@class='nav-link']")).click();
+        Thread.sleep(1200);
+        driver.findElement(By.xpath("(//button[@class='btn btn-sm btn-primary'])[1]")).click();
+        Thread.sleep(1200);
+        driver.findElement(By.xpath("//select[@class='btn btn-outline-primary ng-untouched ng-pristine ng-valid']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@value='company']")).click();
+        Thread.sleep(1500);
+        driver.findElement(By.xpath("//*[@class='form-control form-control-alternative']")).clear();
+        Thread.sleep(1500);
+        driver.findElement(By.xpath("//*[@class='form-control form-control-alternative']")).sendKeys("Dormakaba");
+        Thread.sleep(1500);
+
+        driver.findElement(By.xpath("//button[@class='btn btn-outline-dark']")).click();
+
+        driver.findElement(By.xpath(Values.getMenuButtonXpath())).click();
+        Thread.sleep(1500);
+        driver.findElement(By.xpath(Values.getLogoutButtonXpath())).click();
+        driver.close();
+
+    }
+    @Test(priority = 6)
+    public static void restoreCompany() throws InterruptedException {
+        System.setProperty(Values.getChromeDriver(), Values.getChromeDriverPath());
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://muhasebe-deploy.herokuapp.com/admin");
+        if (driver.getCurrentUrl().endsWith("admin")) {
+            System.out.println(Values.getUrlEndsWithLoginMessage());
+        }
+        else {
+            System.out.println(Values.getUrlDoesNotEndsWithLoginMessage());
+        }
+        driver.findElement(By.xpath(xpath + "'username']")).sendKeys("7840266351");
+        driver.findElement(By.xpath(xpath + "'password']")).sendKeys(Values.getValidPassword());
+        driver.findElement(By.xpath(Values.getLoginButtonXpath())).click();
+        Thread.sleep(3000);
+        if (driver.getCurrentUrl().endsWith("admin-dashboard")) {
+            System.out.println(Values.getSuccessfulLoginMessage());
+        }
+        else {
+            System.out.println(Values.getLoginErrorMessage());
+        }
+        driver.findElement(By.xpath("//a[@class='nav-link']")).click();
+        Thread.sleep(1200);
+        driver.findElement(By.xpath("(//button[@class='btn btn-sm btn-primary'])[1]")).click();
+        Thread.sleep(1200);
+        driver.findElement(By.xpath("//select[@class='btn btn-outline-primary ng-untouched ng-pristine ng-valid']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@value='company']")).click();
+        Thread.sleep(1500);
+        driver.findElement(By.xpath("//*[@class='form-control form-control-alternative']")).clear();
+        Thread.sleep(1500);
+        driver.findElement(By.xpath("//*[@class='form-control form-control-alternative']")).sendKeys("Ronwell");
+        Thread.sleep(1500);
+
+        driver.findElement(By.xpath("//button[@class='btn btn-outline-dark']")).click();
+
+        driver.findElement(By.xpath(Values.getMenuButtonXpath())).click();
+        Thread.sleep(1500);
+        driver.findElement(By.xpath(Values.getLogoutButtonXpath())).click();
+        driver.close();
+
+    }
+    @Test(priority = 7)
+    public static void changePhoneNumber() throws InterruptedException {
+        System.setProperty(Values.getChromeDriver(), Values.getChromeDriverPath());
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://muhasebe-deploy.herokuapp.com/admin");
+        if (driver.getCurrentUrl().endsWith("admin")) {
+            System.out.println(Values.getUrlEndsWithLoginMessage());
+        }
+        else {
+            System.out.println(Values.getUrlDoesNotEndsWithLoginMessage());
+        }
+        driver.findElement(By.xpath(xpath + "'username']")).sendKeys("7840266351");
+        driver.findElement(By.xpath(xpath + "'password']")).sendKeys(Values.getValidPassword());
+        driver.findElement(By.xpath(Values.getLoginButtonXpath())).click();
+        Thread.sleep(3000);
+        if (driver.getCurrentUrl().endsWith("admin-dashboard")) {
+            System.out.println(Values.getSuccessfulLoginMessage());
+        }
+        else {
+            System.out.println(Values.getLoginErrorMessage());
+        }
+        driver.findElement(By.xpath("//a[@class='nav-link']")).click();
+        Thread.sleep(1200);
+        driver.findElement(By.xpath("(//button[@class='btn btn-sm btn-primary'])[1]")).click();
+        Thread.sleep(1200);
+        driver.findElement(By.xpath("//select[@class='btn btn-outline-primary ng-untouched ng-pristine ng-valid']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@value='phonenumber']")).click();
+        Thread.sleep(1500);
+        driver.findElement(By.xpath("//*[@class='form-control form-control-alternative']")).clear();
+        Thread.sleep(1500);
+        driver.findElement(By.xpath("//*[@class='form-control form-control-alternative']")).sendKeys("05342561545");
+        Thread.sleep(1500);
+
+        driver.findElement(By.xpath("//button[@class='btn btn-outline-dark']")).click();
+
+        driver.findElement(By.xpath(Values.getMenuButtonXpath())).click();
+        Thread.sleep(1500);
+        driver.findElement(By.xpath(Values.getLogoutButtonXpath())).click();
+        driver.close();
+
+    }
+    @Test(priority = 8)
+    public static void restorePhoneNumber() throws InterruptedException {
+        System.setProperty(Values.getChromeDriver(), Values.getChromeDriverPath());
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://muhasebe-deploy.herokuapp.com/admin");
+        if (driver.getCurrentUrl().endsWith("admin")) {
+            System.out.println(Values.getUrlEndsWithLoginMessage());
+        }
+        else {
+            System.out.println(Values.getUrlDoesNotEndsWithLoginMessage());
+        }
+        driver.findElement(By.xpath(xpath + "'username']")).sendKeys("7840266351");
+        driver.findElement(By.xpath(xpath + "'password']")).sendKeys(Values.getValidPassword());
+        driver.findElement(By.xpath(Values.getLoginButtonXpath())).click();
+        Thread.sleep(3000);
+        if (driver.getCurrentUrl().endsWith("admin-dashboard")) {
+            System.out.println(Values.getSuccessfulLoginMessage());
+        }
+        else {
+            System.out.println(Values.getLoginErrorMessage());
+        }
+        driver.findElement(By.xpath("//a[@class='nav-link']")).click();
+        Thread.sleep(1200);
+        driver.findElement(By.xpath("(//button[@class='btn btn-sm btn-primary'])[1]")).click();
+        Thread.sleep(1200);
+        driver.findElement(By.xpath("//select[@class='btn btn-outline-primary ng-untouched ng-pristine ng-valid']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@value='phonenumber']")).click();
+        Thread.sleep(1500);
+        driver.findElement(By.xpath("//*[@class='form-control form-control-alternative']")).clear();
+        Thread.sleep(1500);
+        driver.findElement(By.xpath("//*[@class='form-control form-control-alternative']")).sendKeys("5555555555");
+        Thread.sleep(1500);
+
+        driver.findElement(By.xpath("//button[@class='btn btn-outline-dark']")).click();
+
+        driver.findElement(By.xpath(Values.getMenuButtonXpath())).click();
+        Thread.sleep(1500);
+        driver.findElement(By.xpath(Values.getLogoutButtonXpath())).click();
+        driver.close();
+
+    }
+
+    @Test(priority = 9)
     public static void changeAddress() throws InterruptedException {
         System.setProperty(Values.getChromeDriver(), Values.getChromeDriverPath());
         WebDriver driver = new ChromeDriver();
@@ -189,7 +453,7 @@ public class AdminTablesPage {
 
     }
 
-    @Test(priority = 4)
+    @Test(priority = 10)
     public static void changeEmail() throws InterruptedException {
         System.setProperty(Values.getChromeDriver(), Values.getChromeDriverPath());
         WebDriver driver = new ChromeDriver();
@@ -254,7 +518,7 @@ public class AdminTablesPage {
 
     }
 
-    @Test(priority = 5)
+    @Test(priority = 11)
     public static void changeActiveUntil() throws InterruptedException {
         System.setProperty(Values.getChromeDriver(), Values.getChromeDriverPath());
         WebDriver driver = new ChromeDriver();
@@ -326,4 +590,6 @@ public class AdminTablesPage {
         driver.close();
 
     }
+
+
 }
